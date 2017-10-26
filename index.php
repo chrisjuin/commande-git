c2e4536f4b40a47e98fdc0a48ce4345131c27173
chrisjuin <c.juin.cj@gmail.com>
2017-10-26 11:56:07 +0200

    ajout de note

--- index.php
+++ index.php
@@ -3,3 +3,9 @@
 echo "je ne suis pas une erreur" ; 
 
 ?>
+
+<!-- git log : pour voir tous les commits -->
+
+<!-- git reset --force "numero du commit " : reviens au commit saisie -->
+
+<!-- git push -u origin "nom de la branche --force" : valider la suppression des commits -->
